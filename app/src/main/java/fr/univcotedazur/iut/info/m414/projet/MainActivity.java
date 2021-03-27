d4b5b40c12d970044838c82148f2c0058c3ed0e3
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-03-27 17:57:16 +0100

    Ajout dossier

---
+++ app/src/main/java/fr/univcotedazur/iut/info/m414/projet/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.univcotedazur.iut.info.m414.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
