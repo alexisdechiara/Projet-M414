54043981792aaa90da240ba2e7b47b1681aa6449
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-03-28 12:08:43 +0200

    Ajout exemples d'addition

--- app/src/main/java/fr/univcotedazur/iut/info/m414/projet/AdditionLessonActivity.java
+++ app/src/main/java/fr/univcotedazur/iut/info/m414/projet/AdditionLessonActivity.java
@@ -3,12 +3,19 @@ package fr.univcotedazur.iut.info.m414.projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ToggleButton;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class AdditionLessonActivity extends AppCompatActivity {
 
     YouTubePlayerView youTubePlayerView;
+    ToggleButton exempleButton1;
+    LinearLayout exempleView1;
+    ToggleButton exempleButton2;
+    LinearLayout exempleView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,27 @@ public class AdditionLessonActivity extends AppCompatActivity {
 
         youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
+
+        exempleButton1 = findViewById(R.id.example1);
+        exempleView1 = findViewById(R.id.exempleView1);
+        exempleButton2 = findViewById(R.id.example2);
+        exempleView2 = findViewById(R.id.exempleView2);
+
+        exempleButton1.setOnClickListener(v -> {
+            if(exempleButton1.isChecked()){
+                exempleView1.setVisibility(View.VISIBLE);
+            }else{
+                exempleView1.setVisibility(View.GONE);
+            }
+        });
+
+        exempleButton2.setOnClickListener(v -> {
+            if(exempleButton2.isChecked()){
+                exempleView2.setVisibility(View.VISIBLE);
+            }else{
+                exempleView2.setVisibility(View.GONE);
+            }
+        });
 
     }
 }
