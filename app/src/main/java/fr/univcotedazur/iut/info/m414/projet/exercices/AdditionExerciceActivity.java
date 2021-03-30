00dc7665f2957124a613854a3ce4e757946bdae2
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-03-30 11:59:26 +0200

    Amélioration de addition

--- app/src/main/java/fr/univcotedazur/iut/info/m414/projet/exercices/AdditionExerciceActivity.java
+++ app/src/main/java/fr/univcotedazur/iut/info/m414/projet/exercices/AdditionExerciceActivity.java
@@ -1,10 +1,14 @@
 package fr.univcotedazur.iut.info.m414.projet.exercices;
 
+import android.animation.ObjectAnimator;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,49 +20,104 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import fr.univcotedazur.iut.info.m414.projet.R;
 
 public class AdditionExerciceActivity extends AppCompatActivity {
+
+    private Button confirm;
+    private ArrayList<EditText> result = new ArrayList<>();
+    private ArrayList<TextView> a = new ArrayList<>();
+    private ProgressBar progress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addition_exercice);
         Random r = new Random();
+        result.add(findViewById(R.id.addition_result_1));
+        result.add(findViewById(R.id.addition_result_2));
+        result.add(findViewById(R.id.addition_result_3));
+        result.add(findViewById(R.id.addition_result_4));
+        result.add(findViewById(R.id.addition_result_5));
+        result.add(findViewById(R.id.addition_result_6));
+        result.add(findViewById(R.id.addition_result_7));
+        result.add(findViewById(R.id.addition_result_8));
+        result.add(findViewById(R.id.addition_result_9));
+        result.add(findViewById(R.id.addition_result_10));
+        progress = findViewById(R.id.progressBar);
 
-        TextView a1 = (TextView) findViewById(R.id.addition_1);
-        a1.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        new MultiTextWatcher()
+                .registerEditText(result.get(0))
+                .registerEditText(result.get(1))
+                .registerEditText(result.get(2))
+                .registerEditText(result.get(3))
+                .registerEditText(result.get(4))
+                .registerEditText(result.get(5))
+                .registerEditText(result.get(6))
+                .registerEditText(result.get(7))
+                .registerEditText(result.get(8))
+                .registerEditText(result.get(9))
+                .setCallback(new MultiTextWatcher.TextWatcherWithInstance() {
+                    @Override
+                    public void beforeTextChanged(EditText editText, CharSequence s, int start, int count, int after) {
+                    }
 
-        TextView a2 = (TextView) findViewById(R.id.addition_2);
-        a2.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+                    @Override
+                    public void onTextChanged(EditText editText, CharSequence s, int start, int before, int count) {
+                        checkProgressBar();
 
-        TextView a3 = (TextView) findViewById(R.id.addition_3);
-        a3.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+                    }
 
-        TextView a4 = (TextView) findViewById(R.id.addition_4);
-        a4.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+                    @Override
+                    public void afterTextChanged(EditText editText, Editable editable) {
+                    }
+                });
 
-        TextView a5 = (TextView) findViewById(R.id.addition_5);
-        a5.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_1));
+        a.get(0).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(0).setTextSize(32);
 
-        TextView a6 = (TextView) findViewById(R.id.addition_6);
-        a6.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_2));
+        a.get(1).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(1).setTextSize(32);
 
-        TextView a7 = (TextView) findViewById(R.id.addition_7);
-        a7.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_3));
+        a.get(2).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(2).setTextSize(32);
 
-        TextView a8 = (TextView) findViewById(R.id.addition_8);
-        a8.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_4));
+        a.get(3).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(3).setTextSize(32);
 
-        TextView a9 = (TextView) findViewById(R.id.addition_9);
-        a9.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_5));
+        a.get(4).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(4).setTextSize(32);
 
-        TextView a10 = (TextView) findViewById(R.id.addition_10);
-        a10.setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.add((TextView) findViewById(R.id.addition_6));
+        a.get(5).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(5).setTextSize(32);
 
-        Button confirm = (Button) findViewById(R.id.addition_confirm);
+        a.add((TextView) findViewById(R.id.addition_7));
+        a.get(6).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(6).setTextSize(32);
+
+        a.add((TextView) findViewById(R.id.addition_8));
+        a.get(7).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(7).setTextSize(32);
+
+        a.add((TextView) findViewById(R.id.addition_9));
+        a.get(8).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(8).setTextSize(32);
+
+        a.add((TextView) findViewById(R.id.addition_10));
+        a.get(9).setText(String.valueOf(r.nextInt(100) + 1) + " + " + String.valueOf(r.nextInt(100) + 1));
+        a.get(9).setTextSize(32);
+
+        confirm = (Button) findViewById(R.id.addition_confirm);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +131,7 @@ public class AdditionExerciceActivity extends AppCompatActivity {
                     con.setRequestProperty("content-type", "application/json");
                     con.setRequestProperty("accept", "application/json");
                     con.setDoOutput(true);
-                    String jsonInputString = "{\"expr\": \"" + a1.getText().toString() + "\"}\"";
+                    String jsonInputString = "{\"expr\": \"" + a.get(0).getText().toString() + "\"}\"";
                     OutputStream os = con.getOutputStream();
                     byte[] input = jsonInputString.getBytes();
                     os.write(input, 0, input.length);
@@ -87,7 +146,13 @@ public class AdditionExerciceActivity extends AppCompatActivity {
                     Log.d("ui", e.getMessage());
                 }
             }
-
         });
+    }
+    private void checkProgressBar(){
+        int value = 0;
+        for (EditText e: result) {
+            if(! (e.getText().toString().trim().length() == 0) || ! e.getText().toString().equals("")) value ++;
+        }
+        progress.setProgress(value,true);
     }
 }
